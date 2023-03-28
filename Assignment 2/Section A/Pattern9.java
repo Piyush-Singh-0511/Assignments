40e80ee22796dc33e233e6a78d49ff0c7c3cd6ba
Piyush Mayank Singh <128624793+Piyush-Singh-0511@users.noreply.github.com>
2023-03-28 07:47:04 +0530

    Add files via upload

---
+++ Assignment 2/Section A/Pattern9.java
@@ -0,0 +1,19 @@
+class Pattern9{
+	public static void main(String args[])
+	{
+		int alpha=64;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<6-i;j++)
+			{
+			System.out.print(" ");
+			}
+		for(int j=1;j<=i;j++)
+			{
+				System.out.print(" "+(char)(alpha+j));
+			}
+			System.out.println();
+		}
+    }
+}
+
---
+++ Assignment 2/Section B/Star11.java
@@ -0,0 +1,17 @@
+class Star11{
+	 public static void main(String args[]) {
+		 
+	     for(int i=5;i>=1;i--)             
+		 {
+			 for(int j=0; j<=5-i;j++){
+			 System.out.print(" ");}
+			   
+			for(int k=1;k<=5;k++){
+			System.out.print("*");}
+			
+			System.out.println();
+	     }
+    }
+ 
+}
+
